231deb074131b9fb16b0472201abadddd3ec6eea
Uyitech <ewansihaevans@gmail.com>
2022-08-23 12:58:59 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,28 @@
+function myFunction() {
+    var x = document.getElementById("popup");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+
+function myDrop() {
+    var x = document.getElementById("drop");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+
+function myDropLeft() {
+    var x = document.getElementById("dropLeft");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
